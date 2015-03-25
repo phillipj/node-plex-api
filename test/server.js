b46c021cbdebe78671c69be759945c555dc95204
Phillip Johnsen <johphi@gmail.com>
2015-03-25 18:34:03 +0100

    Minor refactoring of test server util, added suppport for XML files

--- test/server.js
+++ test/server.js
@@ -1,9 +1,8 @@
 var http = require('http');
 var fs = require('fs');
-
 var PLEX_SERVER_PORT = 32400;
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function onRequest(req, res) {
 	var sampleFilename = 'root';
 
 	if (req.url === '/library/sections/1/refresh') {
@@ -15,23 +14,24 @@ var server = http.createServer(function(req, res) {
 		sampleFilename = 'clients';
 	}
 
-	deliverXml(sampleFilename, res);
+	deliverSampleContent(sampleFilename, res);
 });
 
-function deliverXml(filename, response) {
-	fs.readFile('test/samples/'+ filename +'.json', function(err, content) {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-		response.write(content);
-		response.end();
-	});
+function deliverSampleContent(filename, response) {
+	var filepath = hasExtension(filename) ? filename : filename + '.json';
+	fs.createReadStream('test/samples/'+ filepath).pipe(response);
+}
+
+function hasExtension(filename) {
+	return filename.indexOf('.') !== -1;
 }
 
 module.exports = {
-	start: function (port) {
+	start: function start(port) {
 		server.listen(port || PLEX_SERVER_PORT);
 	},
 
-	stop: function() {
+	stop: function stop() {
 		server.close();
 	}
 };
