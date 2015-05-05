cc1f60f889fcc7a1db49d4fe619b8fb6d2bc36dd
Youri Tolstoy <ytolstoy@gmail.com>
2015-05-05 22:17:25 +0200

    feat(api): handling queries responding with something else than JSON

---
+++ test/image-test.js
@@ -0,0 +1,26 @@
+var expect = require('expect.js');
+var server = require('./server');
+
+var PlexAPI = require('..');
+
+describe('query()', function() {
+	var api;
+
+	beforeEach(function() {
+		server.start({
+			contentType: 'image/jpg'
+		});
+
+		api = new PlexAPI('localhost');
+	});
+
+	afterEach(server.stop);
+
+
+	it('resource endpoint should return a buffer', function() {
+		return api.query('/resources/movie-creative-commons-flowercat.jpg').then(function(result) {
+			expect(result).to.be.a(Buffer);
+		});
+	});
+
+});
