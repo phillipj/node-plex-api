b22daeddda14abb681f0464b96e786c2cd2f10f7
Youri Tolstoy <ytolstoy@gmail.com>
2015-05-10 20:35:17 +0200

    Allow me to run test & set default Content-Type answer for nock

---
+++ test/image-test.js
@@ -0,0 +1,30 @@
+var expect = require('expect.js');
+var server = require('./server');
+
+var PlexAPI = require('..');
+
+describe('query()', function() {
+	var api;
+
+	beforeEach(function() {
+		server.start().defaultReplyHeaders({
+			'Content-Type': 'image/jpg'
+		});
+
+		api = new PlexAPI('localhost');
+	});
+
+	afterEach(server.stop);
+
+
+	it.only('resource endpoint should return a buffer', function() {
+		return api.query('/resources/movie-creative-commons-flowercat.jpg').then(function(result) {
+			// throw new Error(typeof result);
+			console.log(result);
+			expect(result).to.be.a(Buffer);
+		},function(e) {
+			console.log('teste', e);
+		});
+	});
+	
+});
